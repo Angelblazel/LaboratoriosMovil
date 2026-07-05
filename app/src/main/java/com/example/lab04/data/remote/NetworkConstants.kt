e66a349ae1e47bab42a52ca513f81da9b9e5a527
Angelblazel <angelobregon1997@hotmail.com>
2026-07-04 20:54:04 -0500

    Laboratorio 08 - Obregon

--- app/src/main/java/com/example/lab04/data/remote/NetworkConstants.kt
+++ app/src/main/java/com/example/lab04/data/remote/NetworkConstants.kt
@@ -5,4 +5,6 @@ object NetworkConstants {
     // const val BASE_URL  = "http://illarek.org/"
 
     const val PROJECT_SLUG = "c22200161"
+
+    const val GOOGLE_WEB_CLIENT_ID = "656779127918-m8shqlro4gjf6mks46d27k0mm09mjah6.apps.googleusercontent.com"
 }
